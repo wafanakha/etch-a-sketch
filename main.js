b51a74c48982ae0aa1bfd1ba8d550c10fd66e763
wafanakha <wafanakha15@gmail.com>
2023-11-08 21:39:46 +0700

    grid system applied

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const container = document.querySelector("#flex-container");
-
-for (let i = 0; i < 64; i++) {
-  var div = document.createElement("div");
+let pixLength = 64;
+for (let i = 0; i < pixLength ** 2; i++) {
+  const div = document.createElement("div");
   div.classList.add("pix");
   container.appendChild(div);
-  console.log(div);
 }
+
+const pix = document.querySelectorAll(".pix");
+pix.forEach((element) => {
+  element.addEventListener(
+    "mouseover",
+    () => (element.style.backgroundColor = "black")
+  );
+});
